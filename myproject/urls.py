bbcfe153bf8025a072e5995c266f5b7d32839067
Admin <admin@example.com>
2026-06-17 02:04:24 +0800

    Remove catch-all route to fix API routing

--- myproject/urls.py
+++ myproject/urls.py
@@ -2,16 +2,9 @@
 URL configuration for myproject project.
 """
 from django.contrib import admin
-from django.urls import path, re_path, include
-from .views import index
-from django.conf import settings
-from django.conf.urls.static import static
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('management.urls')),
-    re_path(r'^(?!admin/|api/).*$', index, name='vue-route'),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
